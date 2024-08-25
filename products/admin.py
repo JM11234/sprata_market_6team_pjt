3f46ea590bcf3bfb4ee74b5d4cf8671aa2749245
DESKTOP-NK05NPD\User <gyum9779@naver.com>
2024-08-25 20:07:03 +0900

    admin페이지 작성

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Products, ProductComment, Hashtag
 # Register your models here.
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProductComment)
+class ProductsCommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Hashtag)
+class HashtagAdmin(admin.ModelAdmin):
+    pass
